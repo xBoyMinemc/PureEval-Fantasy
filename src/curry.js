bdfd8d41f41b44d42a511e6ac5a8441118e18fd5
xBoyMinemc <86919167+xBoyMinemc@users.noreply.github.com>
2023-05-18 22:57:45 +0800

    roll curry.js

--- src/curry.js
+++ src/curry.js
@@ -1,16 +1,14 @@
 import { summon } from './summon.js';
 
-function curry(fun){
+function curry(fun) {
 	if (fun.length < 2) return fun;
 	const result = summon(fun.length, (...args) => {
 		if (args.length >= fun.length) return fun.call(null, ...args);
 		else return curry(fun.bind(null, ...args));
 	});
 	result.origin = fun;
-	// result.len = 0;
 	return result;
 }
-
 
 function uncurry(fun) {
 	return fun.origin;
