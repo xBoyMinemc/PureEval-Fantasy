6333d25002c1a1d21bf7feff527b8e12d290986f
xBoyMinemc <86919167+xBoyMinemc@users.noreply.github.com>
2023-05-18 11:21:47 +0800

    [yume] summon and curry
    yume的意思是梦，我认为与Fantasy近似，就保留采用了

--- src/curry.js
+++ src/curry.js
@@ -1,14 +1,26 @@
 import { summon } from './summon.js';
 
-function curry(fun) {
+function _curry(fun) {
 	if (fun.length < 2) return fun;
 	const result = summon(fun.length, (...args) => {
 		if (args.length >= fun.length) return fun.call(null, ...args);
 		else return curry(fun.bind(null, ...args));
 	});
 	result.origin = fun;
+	// result.len = 0;
 	return result;
 }
+
+//我不知道，但使用两个参数位传递工具函数本身与被柯里化函数，这或许是性能危机的第一根稻草
+function curry(fn) {
+	const result = summon(fn.length, fn);
+	//我不知道下面这行摆在哪里合适，就摆过来了
+	Reflect.defineProperty( result, 'origin', {get 'value'(){return fn}});//  为了uncurry ,但我认为bind依旧相当不雅
+	return result;
+}
+
+
+
 
 function uncurry(fun) {
 	return fun.origin;
--- src/summon.js
+++ src/summon.js
@@ -13,19 +13,31 @@ function _summon(total, fn) {
 		...summonArray(total + 1),
 		'return a0.apply(this,Array.prototype.splice.call(arguments,1));'
 	).bind(null, fn);
-}
+} 
 
+// 不雅 
+// 虽然我认为已经是相当好的情况，参数没有每次传递给原函数，构造一个绑定后的函数
+//
 function summon(total,fn){
-	const Yume =  ((Yume,fn,...args)=>(
-			args.length
-			?(
-				total > args.length//还缺点参数
-				?Object.defineProperties(Yume.bind(null,Yume,fn,...args), {'length': { 'value': total-args.length },'origin':{get value(){return fn.bind(null,...args)}}}) // 把新参数塞进新函数，写上正常length与uncurry用的origin
-				:fn.apply(null,args) //忘记该写什么了，反正参数够了，通过apply带参调用
-			):Yume // 传入参数个数为0 不做变动
-		)) //魔法~ 为我而存在~
-		return Object.defineProperties(Yume.bind(null,Yume,fn), {'length': { 'value': total },'origin':{get value(){return fn }}}) //为了length and curry
-}
+	//total  预期接受的参数总量,fn  量够后调用的函数
+	let Yume = ((Yume, fn, ...args) => (
+		args.length
+		?(
+			total > args.length //已接收参数量 与 预期的参数量
+			?(  //量不够
+				Yume = Yume.bind(null, Yume, fn, ...args), //  把新旧参数都混上
+				Reflect.defineProperty( Yume, 'length',  {     'value': total-args.length }),  // 为了length
+				Reflect.defineProperty( Yume, 'origin',  { get 'value'(){return fn.bind(null,...args)}}), //  为了uncurry ,但我认为bind依旧相当不雅
+				Yume //返回包装好贴上Yume标签的Yume工具函数
+			):fn.apply(null, args) //参数够了，直接apply传参调用
+			):Yume  //我不知道这是否符合规范，但参数不足情况下给个空参返回什么都不做的yume应该很合理
+	  ));
+	  Yume = Yume.bind(null, Yume, fn);
+	  
+	  Reflect.defineProperty( Yume, 'length', {    'value': total  });    // 为了length
+	//   Reflect.defineProperty( Yume, 'origin', {get 'value'(){return fn}});//  为了uncurry ,但我认为bind依旧相当不雅
+	  return Yume
+	}
 
 
 
