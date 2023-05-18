666bd8ff8fb2614d30492ec5e37d3cd7ea3fa8bc
xBoyMinemc <86919167+xBoyMinemc@users.noreply.github.com>
2023-05-18 22:02:07 +0800

    Update curry.js
    返璞归真

--- src/curry.js
+++ src/curry.js
@@ -1,6 +1,6 @@
 import { summon } from './summon.js';
 
-function _curry(fun) {
+function curry(fun){
 	if (fun.length < 2) return fun;
 	const result = summon(fun.length, (...args) => {
 		if (args.length >= fun.length) return fun.call(null, ...args);
@@ -10,17 +10,6 @@ function _curry(fun) {
 	// result.len = 0;
 	return result;
 }
-
-//我做错了一些事情，等待新的推送
-//我不知道，但使用两个参数位传递工具函数本身与被柯里化函数，这或许是性能危机的第一根稻草
-function curry(fn) {
-	const result = summon(fn.length, fn);
-	//我不知道下面这行摆在哪里合适，就摆过来了
-	Reflect.defineProperty( result, 'origin', {get 'value'(){return fn}});//  为了uncurry ,但我认为bind依旧相当不雅
-	return result;
-}
-
-
 
 
 function uncurry(fun) {
