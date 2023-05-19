a51f6fc0cc31753f0cec32a682b0dc0fb5b7426b
xBoyMinemc <86919167+xBoyMinemc@users.noreply.github.com>
2023-05-19 23:52:04 +0800

    format summon.js

--- src/summon.js
+++ src/summon.js
@@ -1,14 +1,14 @@
 // 不雅
 // 虽然我认为已经是相当好的情况
 //
-function summon(total,fn){
+function summon(total, fn) {
 	//total  预期接受的参数总量,fn  量够后调用的函数
 	const Yume = (...args) => (
-		Reflect.defineProperty( fn, 'length', {    'value': total-args.length  }), // <0?
-		fn.apply(null,args)
+		Reflect.defineProperty(fn, 'length', { 'value': total - args.length }), // <0?
+		fn.apply(null, args)
 	);
 
-	Reflect.defineProperty( Yume, 'length', {    'value': total  });    // 为了length
+	Reflect.defineProperty(Yume, 'length', { 'value': total });    // 为了length
 
 	return Yume;
 }
